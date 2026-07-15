b2fa8c2838739f48f425c915d871a35398358880
Andrew-Sor <133566800+Andrew-Sor@users.noreply.github.com>
2026-07-15 12:23:39 +0300

    fix

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
             index = 0; // Если нажали "Вперед" на последнем слайде, переходим к первому
         }
         currentIndex = index;
-        slides.css("transform", "translateX(100%)"); // Смещение слайдов
+        slides.css("transform", `translateX(${-index * 100}%)`); // Смещение слайдов
     }
 
     // Обработчики кликов для кнопок
